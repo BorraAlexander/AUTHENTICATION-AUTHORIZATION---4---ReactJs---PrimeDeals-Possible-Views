d6129bc83a20a9799b6b0718723d7bc4ca30cace
alex <alexking488@gmail.com>
2024-09-21 13:38:47 +0000

    first commit

---
+++ src/components/Products/index.js
@@ -0,0 +1,19 @@
+import AllProductsSection from '../AllProductsSection'
+
+import PrimeDealsSection from '../PrimeDealsSection'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <PrimeDealsSection />
+      <AllProductsSection />
+    </div>
+  </>
+)
+
+export default Products
